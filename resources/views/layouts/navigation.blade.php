ae552de7147fe714452abfdcc748defbee036b2e
prcsmrqz <prcsmrqz0909@gmail.com>
2025-08-26 12:03:12 +0800

    fix: navigation tab UI

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex flex-col flex-1 relative p-4 ">
     <!-- Hamburger button (only on small screens) -->
-    <div class="lg:hidden flex items-center justify-between bg-gray-800 text-white mb-4">
-        <button @click="$dispatch('toggle-sidebar')" class="text-gray-200 hover:text-white focus:outline-none">
+    <div class="lg:hidden flex items-center justify-between bg-white text-black mb-4">
+        <button @click="$dispatch('toggle-sidebar')" class=" focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M4 5h16v2H4V5zm0 6h16v2H4v-2zm0 6h16v2H4v-2z" />
             </svg>
@@ -11,60 +11,60 @@
 
     <!-- Sidebar content -->
     <div :class="{ 'block': open, 'hidden': !open }"
-        class="lg:flex flex-1 flex-col p-2 bg-gray-800 text-white overflow-y-auto min-h-0">
+        class="lg:flex flex-1 flex-col p-2 bg-white text-black overflow-y-auto min-h-0">
 
-        <a href="{{ route('dashboard') }}" class="flex items-center justify-center mb-8">
-            <x-application-logo class="block w-auto fill-current mr-4 text-gray-800 dark:text-gray-200" />
-            <span class="text-2xl font-black"> Money Tracker </span>
+        <a href="{{ route('dashboard') }}" class="flex items-center justify-center mb-5">
+            <x-application-logo class="block w-auto fill-current mr-4 dark:text-gray-200" />
+            <span class="text-xl font-black"> Money Tracker </span>
         </a>
 
-        <div class="mb-5 bg-gray-300 rounded-sm p-2 text-black">
-            <h1 class="text-center font-black text-3xl">{{ Auth::user()->currency_symbol }}
+        <div class="mb-5 bg-gray-300 rounded-md p-2">
+            <h1 class="text-center font-black text-xl lg:text-3xl">{{ Auth::user()->currency_symbol }}
                 {{ floor($remainingIncome) != $remainingIncome
                     ? number_format($remainingIncome, 2)
                     : number_format($remainingIncome, 0) }}
             </h1>
-            <h1 class="text-center text-sm">Remaining Income</h1>
+            <h1 class="text-center text-xs lg:text-sm">Remaining Income</h1>
         </div>
 
         <div class="flex flex-col flex-1">
             <a href="{{ route('transaction.index') }}"
-                class="flex items-center justify-center w-full mb-10 bg-gray-300 font-black text-black p-2 text-center rounded-sm hover:bg-gray-400 transition duration-200">
+                class="flex items-center justify-center w-full mb-10 bg-gray-300 font-black text-sm lg:text-lg p-2 text-center rounded-md hover:bg-gray-400 transition duration-200">
                 <x-heroicon-o-plus class="w-4 h-4 mr-2" />
                 Add Transaction
             </a>
 
             <a href="{{ route('dashboard') }}"
-                class="flex items-center justify-start font-black p-2 text-2xl rounded-sm hover:bg-gray-600 hover:rounded-md transition duration-200 {{ request()->segment(1) == 'dashboard' ? 'bg-white text-gray-900 hover:text-white' : 'text-white' }} ">
-                <x-heroicon-s-home class="w-10 h-10 mr-4 shrink-0" />
+                class="flex items-center justify-start font-medium p-2 text-xl rounded-md hover:bg-gray-300 transition duration-200 {{ request()->segment(1) == 'dashboard' ? 'bg-gray-700 text-white hover:text-black' : '' }} ">
+                <x-heroicon-s-home class="w-8 h-8 mr-4 shrink-0" />
                 Dashboard
             </a>
 
             <a href="{{ route('income.index') }}"
-                class="flex items-center justify-start font-black p-2 text-2xl rounded-sm hover:bg-gray-600 hover:rounded-md transition duration-200 {{ request()->segment(1) == 'income' ? 'bg-white text-gray-900 hover:text-white' : 'text-white' }}">
-                <x-heroicon-s-banknotes class="w-10 h-10 mr-4 text-blue-500" />
+                class="flex items-center justify-start font-medium p-2 text-xl rounded-md hover:bg-gray-300 transition duration-200 {{ request()->segment(1) == 'income' ? 'bg-gray-700 text-white hover:text-black' : '' }}">
+                <x-heroicon-s-banknotes class="w-8 h-8 mr-4 text-blue-400 " />
                 Income
             </a>
 
             <a href="{{ route('expenses.index') }}"
-                class="flex items-center justify-start font-black p-2 text-2xl rounded-sm hover:bg-gray-600 hover:rounded-md transition duration-200 {{ request()->segment(1) == 'expenses' ? 'bg-white text-gray-900 hover:text-white' : 'text-white' }}">
-                <x-heroicon-s-credit-card class="w-10 h-10 mr-4 text-red-500" />
+                class="flex items-center justify-start font-medium p-2 text-xl rounded-md hover:bg-gray-300 transition duration-200 {{ request()->segment(1) == 'expenses' ? 'bg-gray-700 text-white hover:text-black' : '' }}">
+                <x-heroicon-s-credit-card class="w-8 h-8 mr-4 text-red-400" />
                 Expenses
             </a>
 
             <a href="{{ route('savings.index') }}"
-                class="flex items-center justify-start font-black p-2 text-2xl rounded-sm hover:bg-gray-600 hover:rounded-md transition duration-200 {{ request()->segment(1) == 'savings' ? 'bg-white text-gray-900 hover:text-white' : 'text-white' }}">
-                <x-lucide-piggy-bank class="w-10 h-10 mr-4 text-green-500" />
+                class="flex items-center justify-start font-medium p-2 text-xl rounded-md hover:bg-gray-300 transition duration-200 {{ request()->segment(1) == 'savings' ? 'bg-gray-700 text-white hover:text-black' : '' }}">
+                <x-lucide-piggy-bank class="w-8 h-8 mr-4 text-green-400" />
                 Savings
             </a>
 
             <a href="{{ route('dashboard') }}"
-                class="flex items-center justify-start font-black p-2 mb-5 text-2xl rounded-sm hover:bg-gray-600 hover:rounded-md transition duration-200 {{ request()->segment(1) == 'settings' ? 'bg-white text-gray-900 hover:text-white' : 'text-white' }}">
-                <x-heroicon-s-cog-8-tooth class="w-10 h-10 mr-4 text-gray-400" />
+                class="flex items-center justify-start font-medium p-2 mb-5 text-xl rounded-md hover:bg-gray-300 transition duration-200 {{ request()->segment(1) == 'settings' ? 'bg-gray-700 text-white hover:text-black' : '' }}">
+                <x-heroicon-s-cog-8-tooth class="w-8 h-8 mr-4 text-gray-400" />
                 Settings
             </a>
 
-            <hr class="mt-auto mb-5" />
+            <hr class="mt-auto border-black" />
         </div>
 
         <!-- Bottom section -->
@@ -87,9 +87,8 @@
 
         <div class="flex items-center w-full mt-5 mb-5 px-1 cursor-pointer">
             <x-dropdown align="right" width="full">
-                <x-slot name="trigger" class="flex items-center justify-start rounded-sm">
-                    <img src="{{ asset('/default-avatar.png') }}" class="w-10 h-10 rounded-full mr-3"
-                        alt="User Avatar">
+                <x-slot name="trigger" class="flex items-center justify-start rounded-md">
+                    <img src="{{ asset('/default-avatar.png') }}" class="w-8 h-8 rounded-full mr-3" alt="User Avatar">
                     <h1 class="font-black text-xl">{{ Auth::user()->name }}</h1>
                 </x-slot>
 
