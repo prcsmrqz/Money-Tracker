eaac99368b44f08d77d4f07cc005ee85910c88d4
prcsmrqz <prcsmrqz0909@gmail.com>
2025-08-29 10:45:44 +0800

    feat: landing page

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,16 @@ import Alpine from 'alpinejs';
 import { confirmDelete } from './confirm_delete';
 import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 Chart.register(ChartDataLabels);
+
+AOS.init({
+  duration: 1500,
+  once: false, 
+});
+
 
 window.Chart = Chart; 
 window.Alpine = Alpine;
